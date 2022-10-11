e6fd178929effaf48ed7150117017a5c173e0935
Raven I. Finch <purplebox02@gmail.com>
2022-10-11 19:19:29 +0300

    Bug Fixes for Netlify

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -64,6 +64,7 @@ export default function PrimarySearchAppBar() {
     } else {
       setPageName(strings.pageText);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userLangValue]);
 
   const languageHandler = () => {
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -48,6 +48,7 @@ export default function Sidebar() {
     let sidebarStyle = document.querySelector('.sidebar-container');
     sidebarStyle.style.display = 'none';
     console.log(open);
+    console.log(pageNameValue);
   }
 
   window.addEventListener('resize', function(){
